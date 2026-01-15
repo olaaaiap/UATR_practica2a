f3dfa05b29b07d3fbe28bd87818930e00ed38310
olaaaiap <01opikabea@gmail.com>
2026-01-15 20:18:31 +0100

    UPDATE InputManager. Crear clase GLFWInputManager

--- APIS_2025/InputManager.h
+++ APIS_2025/InputManager.h
@@ -1,26 +1,11 @@
 #pragma once
 #include "mapi/common.h"
 
-typedef struct {
-	double xPos;
-	double yPos;
-	std::map<int, bool> buttonState;
-} mouseStats_t;
-
-
 class InputManager
 {
 public:
-	static inline GLFWwindow* window;
 	static inline std::map<int, bool>keyboardState; //Mapa de carácteres para poder consultar si una tecla está apretada o no
-	static inline mouseStats_t mouseState; //Mouse state
-
-
-	static void init(GLFWwindow* window); //Initialize key manager
-	static void windowKeyboardEvent(GLFWwindow* window, int key, int scancode, int action, int mods); //Keyboard event
-	static void mousePosEvent(GLFWwindow* window, double xpos, double ypos); //Mouse position event
-	static void mouseButtonEvent(GLFWwindow* window, int button, int action, int mods); //Mouse button event
-
-	bool isPressed(char key);
+	virtual void init(GLFWwindow* window) = 0;
+	virtual bool isPressed(char key) = 0;
 };
 
